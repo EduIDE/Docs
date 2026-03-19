a7ac82d3a613cc0032ae94692f85b83c5362246e
lukaskratzel <lukaskratzel2004@gmail.com>
2026-03-19 10:23:55 +0100

    Move base url to '/'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     v4: true,
   },
   url: 'https://EduIDE.github.io',
-  baseUrl: '/Docs/',
+  baseUrl: '/',
   organizationName: 'EduIDE',
   projectName: 'Docs',
   trailingSlash: false,
